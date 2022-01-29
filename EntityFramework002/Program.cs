aaae95d9bf88080528b67cfb210214d9b77a676c
Fieldname <martynas.ven@protonmail.com>
2022-01-29 20:17:08 +0200

    Add project files.

---
+++ EntityFramework002/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new VidzyEntities1();
+            video.AddVideo("Video1", null, "Drama");
+            video.AddVideo("Video2", null, "Comedy");
+            video.AddVideo("Video3", null, "Action");
+        }
+    }
+}
